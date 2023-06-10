ddbb74eb41e381bd07452348fc559f9948b2fc4e
hinkalinka <100852420+hinkalinka@users.noreply.github.com>
2023-06-10 18:31:30 +0300

    Добавление главной страницы

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang=ru>
+	<head>
+		<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<title>Новости</title>
+	</head>
+<body>
+	
+	<header>
+		
+	</header>
+
+	<section>
+		
+	</section>
+
+	<footer>
+		
+	</footer>
+
+</body>
+</html>
